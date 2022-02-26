07041102b15f4dc2056cb2583219807b5ad22a7e
gehad-magdi <55022728+gehad-magdi@users.noreply.github.com>
2022-02-26 04:14:01 +0200

    add some cleaning files and code and add sass

---
+++ todoTest.js
@@ -0,0 +1,75 @@
+function creating(elem,name,to){
+    let create = document.createElement(elem);
+    create.classList.add(name);
+    let selector = document.querySelector(to);
+    selector.appendChild(create);
+}
+creating('div','container','#Note');
+creating('h1','titleNote','.container');
+document.querySelector('.titleNote').innerHTML = "ToDo List";
+creating('form','form','.container');
+creating('input','input','.form');
+let change  = document.querySelector('.input');
+change.setAttribute('type','text');
+change.setAttribute('placeholder','Add to the todo list');
+creating('button','button','.form');
+document.querySelector('.button').innerHTML = 'Add Item';
+creating('p','hint','.form');
+document.querySelector('.hint').innerHTML = 'Please Add Your ToDo';
+change = document.querySelector('.button');
+change.setAttribute('type','submit');
+creating('div','mainTodo','.container');
+// end of
+let outbox = [];
+function draw (){
+    let toDoBox = `toDoBox${outbox.length - 1}`;
+    creating('div',toDoBox,'.mainTodo');
+    document.querySelector(`.${toDoBox}`).classList.add('boxTodo')
+    let todoText = `todoText${outbox.length - 1}`;
+    creating('p',todoText,`.${toDoBox}`);
+    document.querySelector(`.${todoText}`).innerHTML = `${outbox[outbox.length -1]}`;
+    document.querySelector(`.${todoText}`).classList.add('textTodo');
+    let label = `label${outbox.length - 1}`;
+    creating('label',`${label}`,`.${toDoBox}`);
+    document.querySelector(`.${label}`).classList.add('label');
+    let checkbox = `checkbox${outbox.length - 1}`;
+    creating('input',`${checkbox}`,`.${label}`);
+    document.querySelector(`.${checkbox}`).classList.add('checkbox');
+    document.querySelector(`.${checkbox}`).setAttribute('type','checkbox');
+    creating('span','checkmark',`.${label}`);
+    let check = document.querySelector(`.${checkbox}`);
+    check.addEventListener('click',function(){
+            if(this.checked == true){
+                this.parentElement.previousSibling.style.textDecoration = 'line-through';
+            }
+            else{
+                this.parentElement.previousSibling.style.textDecoration = 'none';
+            }
+        });
+    let Delete = `Delete${outbox.length - 1}`;
+    creating('button',`${Delete}`,`.${toDoBox}`);
+    let trash =  document.querySelector(`.${Delete}`);
+    trash.classList.add('delete');
+    trash.innerHTML = '<i class="fa fa-trash-o" style="font-size:24px"></i>';
+    trash.addEventListener('click',function(){
+        this.parentElement.remove();
+        let inclouds = outbox.length;        
+    })
+}
+let form = document.querySelector('.form');
+form.onsubmit = function(e){
+    if(this.firstChild.value.length == 0){
+        this.children[2].style.display = 'block';
+        e.preventDefault();
+    }
+    else{
+        e.preventDefault();
+        this.children[2].style.display = 'none';
+        window.localStorage.setItem('ToDo',this.firstChild.value);
+        outbox.push(window.localStorage.getItem('ToDo'))
+        draw();
+        if(this.firstChild.value.length !== 0){
+            this.firstChild.value = '';
+            }
+    }
+}
